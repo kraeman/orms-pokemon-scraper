7f1bd26a88b20af56e8d5b177074980f374086a9
krae <kraewind@gmail.com>
2020-12-21 16:33:31 -0500

    commit tree de4aea94281e63468822898f18d7c190ced12598

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,36 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+            sql = <<-SQL
+              INSERT INTO pokemon (name, type)
+              VALUES (?, ?)
+            SQL
+            db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = ?
+            LIMIT 1
+        SQL
+        result = db.execute(sql, id)[0]
+        hash = {id: result[0], name: result[1], type: result[2], db: db}
+        self.new(hash)
+    end
+
+    def update
+        sql = "UPDATE pokemon SET name = ?, type = ?, db = ? WHERE id = ?"
+        @db.execute(sql, self.name, self.type, self.db, self.id)
+    end
 end
